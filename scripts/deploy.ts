6a7b4b049563a8c09920ede1ce4b6f1e15252a1b
Devesh Rawat <devsrawt@gmail.com>
2023-02-24 22:14:03 +0530

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,21 +6,6 @@ import hre from "hardhat";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-// async function main() {
-//   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-//   const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-//   const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-//   const lockedAmount = ethers.utils.parseEther("1");
-
-//   const Lock = await ethers.getContractFactory("Lock");
-//   const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-//   await lock.deployed();
-
-//   console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
-// }
-
 const deployeContract = async () => {
   const INFURA_API_KEY = process.env.INFURA_API_KEY;
   const SECRET_PHRASE = process.env.SECRET_PHRASE || "";
